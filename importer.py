0ff7345c71f5a43f086971f6d4a2003bd3d4d364
raylu <lurayl@gmail.com>
2014-07-04 02:13:08 -0700

    importer: prioritize common groups

--- importer.py
+++ importer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import heapq
 import json
 import operator
 import sys
@@ -34,43 +35,43 @@ def main():
 
 		groups = db.query(c, 'SELECT groupID FROM eve.invGroups WHERE categoryID = ?', 6)
 		groups = list(map(operator.itemgetter('groupID'), groups))
-		last_kill_ids = {}
+		last_kill_ids = []
 		for i in range(0, len(groups), 10):
 			query_groups = ','.join(map(str, groups[i:i+10]))
-			last_kill_ids[query_groups] = None
+			heapq.heappush(last_kill_ids, (-float('inf'), query_groups))
 		last_request_time = 0
+		last_kill_id, query_group = heapq.heappop(last_kill_ids)
 		while True:
-			for query_group in last_kill_ids:
-				path = '/api/losses/api-only/groupID/' + query_group
-				last_kill_id = last_kill_ids[query_group]
-				if last_kill_id is not None:
-					path += '/beforeKillID/' + str(last_kill_id)
-				now = time.time()
-				if now - last_request_time < 12:
-					sleep_secs = 12 - (now - last_request_time)
-					print('sleeping', sleep_secs)
-					time.sleep(sleep_secs)
-				last_request_time = time.time()
-				try:
-					r = rs.get('https://zkillboard.com' + path)
-					kills = r.json()
-				except Exception as e:
-					print(repr(e))
-					break
-				print('inserting', len(kills), 'kills', end='... ')
-				sys.stdout.flush()
-				inserted = 0
-				try:
-					for kill in kills:
-						if db.queries.insert_kill(c, kill):
-							inserted += 1
-				except TypeError as e:
-					print(repr(e), kills)
-					break
-				db.conn.commit()
-				print(len(kills) - inserted, 'dupes')
-				last_kill_id = kills[-1]['killID']
-				last_kill_ids[query_group] = last_kill_id
+			path = '/api/losses/api-only/groupID/' + query_group
+			if last_kill_id != -float('inf'):
+				path += '/beforeKillID/' + str(-last_kill_id)
+			now = time.time()
+			if now - last_request_time < 12:
+				sleep_secs = 12 - (now - last_request_time)
+				print('sleeping', sleep_secs)
+				time.sleep(sleep_secs)
+			last_request_time = time.time()
+			try:
+				r = rs.get('https://zkillboard.com' + path)
+				kills = r.json()
+			except Exception as e:
+				print(repr(e))
+				break
+			print('inserting', len(kills), 'kills', end='... ')
+			sys.stdout.flush()
+			inserted = 0
+			try:
+				for kill in kills:
+					if db.queries.insert_kill(c, kill):
+						inserted += 1
+			except TypeError as e:
+				print(repr(e), kills)
+				break
+			db.conn.commit()
+			print(len(kills) - inserted, 'dupes')
+
+			last_kill_id = int(kills[-1]['killID'])
+			last_kill_id, query_group = heapq.heappushpop(last_kill_ids, (-last_kill_id, query_group))
 
 if __name__ == '__main__':
 	main()
